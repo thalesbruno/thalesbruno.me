06116dbec658d964e065f92b736dbb01bc14ba35
Thales Bruno <thalesbrunom@gmail.com>
2019-10-08 19:53:31 -0300

    add webfonts dir, add blog view and start blog template

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import home
+from blog.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('blog/', index)
 ]
