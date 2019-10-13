c1e94e31ee94504dec27b870fefca71d2659cfb5
Thales Bruno <thalesbrunom@gmail.com>
2019-10-13 19:16:51 -0300

    update works section cards

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Technology(models.Model):
     title = models.CharField(max_length=20)
+    badge = models.CharField(max_length=20)
 
     class Meta:
         verbose_name_plural = 'Technologies'
