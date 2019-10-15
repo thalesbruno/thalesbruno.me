e9b5d957f519e59566a1d95fa3cf85cc221480aa
Thales Bruno Lima Malheiro <tblmalheiro@tjba.jus.br>
2019-10-15 11:43:12 -0300

    add blog post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from home.models import Tag
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    thumbnail = models.ImageField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    view_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
